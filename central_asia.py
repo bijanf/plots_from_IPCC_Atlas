707a52a1351effdfe6be66d84f1411262eae5572
bijanf <bijan.fallah@gmail.com>
2025-01-31 11:54:27 +0100

    update the topography plot

--- central_asia.py
+++ central_asia.py
@@ -5,62 +5,82 @@ def plot_topo_only_land():
     Plot topography over land only, assigning any below-sea-level elevations
     to the color bin at 0 m. Oceans and large lakes are shown in solid blue,
     while small lakes are omitted. Labels major Central Asian countries.
-    (No triangular tip on the left side of the color bar due to GMT version constraints.)
     """
 
-    # 1) Define the map region
+    # Define the map region
     minlon, maxlon = 43, 89
     minlat, maxlat = 34, 56
 
     fig = pygmt.Figure()
 
-    # 2) Create a custom CPT from -0.00001 to 8000 m,
-    #    forcing anything < 0 to map to the color at 0 m.
+    # Create a custom CPT with the specified elevation ranges and colors
+
+    # Define the CPT file content
+    def create_custom_cpt():
+        cpt_content = """\
+    # Elevation (m) Color Palette Table (CPT)
+    0 145/167/140 200 145/167/140
+    200 161/184/155 500 161/184/155
+    500 188/206/182 1000 188/206/182
+    1000 214/210/175 2000 214/210/175
+    2000 205/188/139 3000 205/188/139
+    3000 176/161/130 4000 176/161/130
+    4000 214/208/189 5000 214/208/189
+    5000 239/242/239 6000 239/242/239
+    6000 255/255/255 8000 255/255/255
+    B 145/167/140
+    F 250/250/250
+    N 128/128/128
+    """
+        with open("custom.cpt", "w") as f:
+            f.write(cpt_content)
+
+    create_custom_cpt()  # Call this function before using makecpt()
+
+
+    # Use the custom CPT in PyGMT
     pygmt.makecpt(
-        cmap="topo",
-        series="-0.00001/8000/1000",
-        continuous=True
+    cmap="custom.cpt",
+    continuous=True
     )
 
-    # 3) Plot global relief with hillshading; negative elevations
-    #    all use the 0-m color from the CPT.
+
+
+    # Plot global relief with hillshading
     fig.grdimage(
         grid="@earth_relief_30s",
         region=[minlon, maxlon, minlat, maxlat],
         projection="M6i",
-        shading=True,
+        shading=0.05,
         frame=True
     )
 
-    # 4) Paint large water bodies in blue, skipping small lakes <5000 km^2.
+    # Paint large water bodies in the specified color
     fig.coast(
         region=[minlon, maxlon, minlat, maxlat],
         projection="M6i",
-        borders="1/0.5p",
+        borders="1/0.1p",
         shorelines=True,
-        rivers="0-2/1p,blue",
+        rivers="0-2/1p,#5f92b7",  # River network color
         land=None,
-        water="lightblue",
-        lakes="lightblue",
+        water="#cfe6f5",  # Water bodies color
+        lakes="#cfe6f5",
         area_thresh="90000",  # skip small lakes
-        frame=True
+        frame=False
     )
 
-    # 5) Add a color bar (without triangular tip).
-    #    For older GMT (<6.4), "triangle='l'" is unsupported, so we omit that.
+    # Add a color bar with custom elevation labels
     fig.colorbar(
-        frame=['+l"Elevation (m)"']  # label for the color bar
+        frame=['x+lElevation (m)'],  # Label for the color bar
     )
 
-    # 6) Label some major Central Asian countries (approx. coordinates)
+    # Label some major Central Asian countries (approx. coordinates)
     country_positions = {
         "Kazakhstan":   (66, 49),
         "Uzbekistan":   (63, 42),
         "Turkmenistan": (57, 39),
         "Kyrgyzstan":   (74, 42),
         "Tajikistan":   (71, 38),
-    
-    
     }
 
     for country, (cx, cy) in country_positions.items():
@@ -69,11 +89,11 @@ def plot_topo_only_land():
             text=country,
             region=[minlon, maxlon, minlat, maxlat],
             projection="M6i",
-            font="15p,Helvetica,black",
+            font="10p,Helvetica,#000000",  # Labels in black
             justify="CM"
         )
 
-    # 7) Save the figure
+    # Save the figure
     fig.savefig("central_asia_land_topo.pdf", crop=True, dpi=300)
 
 if __name__ == "__main__":
